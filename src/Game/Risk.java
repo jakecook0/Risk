df86ea08bab0d3bb3f1c33a684f8996d8ee26938
Jake Cook <jake.cook@aggiemail.usu.edu>
2019-11-27 13:40:44 -0700

    Final map-style developed

--- src/Game/Risk.java
+++ src/Game/Risk.java
@@ -1,14 +1,15 @@
 package Game;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,43 +17,49 @@ import java.util.Arrays;
 public class Risk extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        int turn = 0;   //turn global
         GridPane gp = new GridPane();
 
         //construct countries in a continent (unreal mapping)
-        //WHAT IF:
-        //load an array into the gridpane? or key:val pairs? (coord:color)
+        //load an array into the gridpane
         //builds each region w/associated color
-        ArrayList boardColors = new ArrayList(20);  //3,3,4 x 2,3,3,2 countries
-//        String[][] lineColorings = new String[][]{
-//                {"blue", "blue", "blue", "red", "red", "red", "green", "green", "green", "green"},
-//                {"blue", "blue", "blue", "red", "red", "red", "green", "green", "green", "green"},
-//                {"blue", "blue", "green", "green", "red", "red", "green", "green", "black", "black"},
-//                {"blue", "green", "green", "green", "blue", "blue", "blue", "green", "black", "black"},
-//                {"red", "green", "green", "green", "blue", "blue", "blue", "black", "black", "black"},
-//                {},
-//        };
+
         Color[][] colors = new Color[][]{   //5x5 color grid BLACK SPACES ARE EMPTY-REGIONS
-                {Color.GREEN, Color.GREEN, Color.GREEN, Color.BLUE, Color.BLUE},
-                {Color.BLACK, Color.GREEN, Color.BLACK, Color.BLACK, Color.BLUE},
-                {Color.BLACK, Color.GREEN, Color.BLUE, Color.BLACK, Color.BLUE},
-                {Color.BLUE, Color.BLUE, Color.GREEN, Color.BLUE, Color.GREEN},
-                {Color.BLACK, Color.BLUE, Color.BLACK, Color.BLACK, Color.GREEN},
+                {Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK},
+                {Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK},
+                {Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK},
+                {Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK},
+                {Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK},
+                {Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK, Color.WHITE},
+                {Color.BLACK, Color.WHITE, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE,},
+                {Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.WHITE,},
+                {Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK},
+                {Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK, Color.WHITE, Color.WHITE, Color.WHITE, Color.BLACK, Color.BLACK, Color.BLACK},
         };
 
         for (int i=0; i<colors.length; i++) {
             for (int j=0; j<colors[i].length; j++) {
-                Rectangle box = new Rectangle(50, 50, colors[j][i]);
+                //add a rectangle to the gridpane with associated color (init at white)
                 if (!colors[j][i].equals(Color.BLACK)) {
+                    Rectangle box = new Rectangle(75, 75, colors[j][i]);
                     box.setStroke(Color.ORANGE);
+                    gp.add(box, i, j);
+                    EventHandler<MouseEvent> clicked = e -> {
+                        Rectangle r = (Rectangle)e.getSource();
+                    };
                 }
-                else box.setStroke(Color.BLACK);
-                gp.add(box, i, j);
+                else {
+                    Rectangle box = new Rectangle(75,75, colors[j][i]);
+                    box.setStroke(Color.BLACK);
+                    gp.add(box, i, j);
+                }
+
             }
         }
         //logic to make border around each region
 
 
-        Scene scene = new Scene(gp, 500, 500);
+        Scene scene = new Scene(gp, 750, 750);
         stage.setTitle("Risk demo");
         stage.setScene(scene);
         stage.show();
