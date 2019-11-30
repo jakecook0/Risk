799c7ec0055f524bffc521d640e6528d678e0670
Jake Cook <jake.cook@aggiemail.usu.edu>
2019-11-29 20:43:09 -0700

    built clickable regions, Stackpane prevents base gpColor from being clicked with the number on top. Add another gp of selections to clear every time. Game Logic updated

--- src/Game/GameLogic.java
+++ src/Game/GameLogic.java
@@ -1,5 +1,43 @@
 package Game;
 
 public class GameLogic {
+    public void gameInit() {    //get one array position
+        //alternates between players to place troops in territories (gives each territory 2? troops)
+        //needs to modify the <colors> and <troops> arrays
 
+    }
+
+
+    public void place() {   //get one array position
+        //calculates how many troops a player should receive (based on % of board controlled)
+        //needs the <colors> and <troops> arrays
+
+    }
+
+
+    public void move() {    //get two array positions
+        //allows player to move troops between own territories (connected preferably, may add that logic later)
+
+    }
+
+
+    public void attack() {  //get two array positions
+        //attack sequence, player selects own block first, then enemy block
+        //uses roll() to determine who wins
+
+    }
+
+
+    private boolean roll() {
+        //one is attacker, two  is defender
+        double one = Math.random()*10%6;
+        double two = Math.random()*10%6;
+        return one > two;   //return true if attacker wins, false if attacker loses
+    }
+
+
+    private void selectTerritories() {  //returns the array positions of the user-selected blocks
+
+
+    }
 }
